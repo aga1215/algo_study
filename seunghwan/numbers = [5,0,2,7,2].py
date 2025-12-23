46bfdae4ec0c6758ae9603e1ea8415c1cb646d29
UserK <renobly@gmail.com>
2025-12-23 19:39:48 +0900

    feat: generate and sort pairwise sums using custom quick sort

---
+++ seunghwan/numbers = [5,0,2,7,2].py
@@ -0,0 +1,26 @@
+numbers = [5,0,2,7,2]
+
+
+def quick_sort(arr):
+    n=len(arr)
+    if n<=1:
+        return arr
+    pivot = arr[n//2]
+    left_ary, right_ary = [], []
+    for number in arr:
+        if number<pivot:
+            left_ary.append(number)
+        elif number>pivot:
+            right_ary.append(number)    
+    return quick_sort(left_ary)+pivot+quick_sort(right_ary)
+
+def solution(numbers):
+    n = len(numbers)
+    temp = []
+    for i in range(1, n):
+        for j in range(i+1, n):
+            temp.append(numbers[i]+numbers[j])
+    answer = quick_sort(temp)
+    return answer
+
+
