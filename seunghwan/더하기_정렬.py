47125892a3fdffb39b2b0574abf910a2c41aac78
renobly-oss <renobly@gmail.com>
2025-12-23 20:10:49 +0900

    Rename file to 더하기_정렬.py

--- seunghwan/더하기_정렬.py
+++ seunghwan/더하기_정렬.py
@@ -6,21 +6,23 @@ def quick_sort(arr):
     if n<=1:
         return arr
     pivot = arr[n//2]
-    left_ary, right_ary = [], []
+    left_ary, mid_ary, right_ary = [], [], []
     for number in arr:
         if number<pivot:
             left_ary.append(number)
         elif number>pivot:
-            right_ary.append(number)    
-    return quick_sort(left_ary)+pivot+quick_sort(right_ary)
+            right_ary.append(number)
+        else:
+            mid_ary.append(number)    
+    return quick_sort(left_ary)+mid_ary+quick_sort(right_ary)
 
 def solution(numbers):
     n = len(numbers)
     temp = []
-    for i in range(1, n):
+    for i in range(n):
         for j in range(i+1, n):
             temp.append(numbers[i]+numbers[j])
+
+    temp = list(set(temp))    
     answer = quick_sort(temp)
     return answer
-
-
