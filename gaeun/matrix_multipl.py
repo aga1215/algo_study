de9345657973c95ffae3ed42c0b56a3882acce04
Lee Gaeun <141293346+baekdmyng@users.noreply.github.com>
2025-12-24 18:40:47 +0900

    [PGS] 행렬의 곱셈 / 레벨2 / 1시간

--- gaeun/matrix_multipl.py
+++ gaeun/matrix_multipl.py
@@ -1,14 +1,13 @@
 def solution(arr1, arr2):
-    arr1_row = len(arr1) # arr1의 행의 개수
-    arr2_col = len(arr2[0]) # arr2의 첫번째 줄 숫자의 개수
-    N = len(arr2) # arr2의 행의 개수
+    arr1_row = len(arr1) # arr1 행의 개수
+    arr2_col = len(arr2[0]) # arr2 열의 개수
+    K = len(arr2) # arr2 행의 개수
     
-    answer = [[0 for _ in range(len(arr2[0]))] for _ in range(len(arr1))] # 결과 행렬 초기화
+    answer = [[0 for _ in range(arr2_col)] for _ in range(arr1_row)]
     
     for i in range(arr1_row):
         for j in range(arr2_col):
-            for k in range(N):
+            for k in range(K):
                 answer[i][j] += arr1[i][k] * arr2[k][j]
-                
     return answer
         
