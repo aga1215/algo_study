db501b89429f27ffc5cc1ea03cac50a1fb8770b3
blueberryade <blueberryade@snu.ac.kr>
2025-12-26 10:30:01 +0900

    [ETC] ArraySort/배열정렬/10분

--- taemin/ArraySort.py
+++ taemin/ArraySort.py
@@ -7,7 +7,7 @@ n = len(arr)
 for i in range(n-1):
     for j in range(n-1-i):
         if arr[j] > arr[j+1]:
-            arr[j], arr[j+1] = a[j+1], arr[j]
+            arr[j], arr[j+1] = arr[j+1], arr[j]
 
 print(arr)
 
